f2ae3c73e17d68f38d4950d8f5826a395e48b509
stefanaritonovic2394 <stefan.aritonovic.2394@metropolitan.ac.rs>
2016-05-25 15:54:29 +0200

    IT255-DZ09-Stefan-Aritonović-2394

---
+++ app/pocetna/pocetna.component.ts
@@ -0,0 +1,9 @@
+import {Component, Directive} from 'angular2/core';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+
+@Component({
+    selector: 'Pocetna',
+    templateUrl: 'app/pocetna/pocetna.html'
+})
+
+export class PocetnaComponent {}

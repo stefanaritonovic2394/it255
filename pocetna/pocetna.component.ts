728470813d4369238189975fe6c60824dc579d8a
stefanaritonovic2394 <stefan.aritonovic.2394@metropolitan.ac.rs>
2016-05-29 14:41:39 +0200

    IT255-DZ10-Stefan-Aritonović-2394

---
+++ pocetna/pocetna.component.ts
@@ -0,0 +1,20 @@
+import {Component, Directive} from 'angular2/core';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import {SearchPipe} from 'app/pipe/search';
+import 'rxjs/Rx';
+
+@Component({
+	pipes: [SearchPipe],
+    selector: 'Pocetna',
+    templateUrl: 'app/pocetna/pocetna.html'
+})
+
+export class PocetnaComponent {
+	broj_kreveta: String = "";
+	sobe: Object[];
+	constructor(http: Http) {
+		http.get('http://localhost/dz10/json_service.php')
+		.map(res => res.json())
+		.subscribe(sobe => this.sobe = sobe);
+	}
+}

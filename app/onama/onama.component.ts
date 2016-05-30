a6513b5bd86188ec6f1de8b51ca4e7dfb3522e91
stefanaritonovic2394 <stefan.aritonovic.2394@metropolitan.ac.rs>
2016-05-30 13:54:58 +0200

    IT255-DZ11-Stefan-Aritonović-2394

---
+++ app/onama/onama.component.ts
@@ -0,0 +1,8 @@
+import {Component, Directive} from 'angular2/core';
+
+@Component({
+    selector: 'Onama',
+    templateUrl: 'app/onama/onama.html'
+})
+
+export class OnamaComponent {}
---
+++ app/rezervacija/rezervacija.component.ts
@@ -0,0 +1,8 @@
+import {Component, Directive} from 'angular2/core';
+
+@Component({
+    selector: 'Rezervacija',
+    templateUrl: 'app/rezervacija/rezervacija.html'
+})
+
+export class RezervacijaComponent {}

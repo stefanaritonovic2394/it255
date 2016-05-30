72f8d13f22cf5f9c900e5664f2ccf008f61b6a4c
stefanaritonovic2394 <stefan.aritonovic.2394@metropolitan.ac.rs>
2016-05-30 13:43:45 +0200

    Uklonjen IT255-DZ09-Stefan-Aritonović-2394

--- app/main.ts
+++
@@ -1,7 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {bind} from 'angular2/core';
-import {Http, Headers, HTTP_PROVIDERS } from 'angular2/http';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {AppComponent} from './app.component';
-
-bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS]);
--- app/onama/onama.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component, Directive} from 'angular2/core';
-
-@Component({
-    selector: 'Onama',
-    templateUrl: 'app/onama/onama.html'
-})
-
-export class OnamaComponent {}
